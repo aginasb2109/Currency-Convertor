ddd1f4ba866fa6c010fe8119bedf756e283b48c8
Aginas B <138473118+aginasb2109@users.noreply.github.com>
2024-10-15 23:18:20 +0530

    Add files via upload

---
+++ currency.js
@@ -0,0 +1,12 @@
+from_currency=document.getElementById("from")
+To_currency=document.getElementById("To")
+result1=document.getElementById("result1")
+result1=document.getElementById("result1")
+btn=document.getElementById("btn")
+
+btn.addEventListener("click", convert)
+
+function convert(){
+    
+}
+
